982d50aa18ad7c09135e9e7d761b775b1bc39540
Martin Szemethy <mszemethy1@sheffield.ac.uk>
2018-10-24 14:13:27 +0100

    Created Eclipse project and FindDrivers (to see if the JDBC MySQL driver is working)

---
+++ src/FindDrivers.java
@@ -0,0 +1,16 @@
+import java.sql.*;
+import java.util.*;
+
+public class FindDrivers {
+
+	public static void main(String[] args) throws Exception {
+	
+		System.out.println("\nDrivers loaded as properties:");
+		System.out.println(System.getProperty("jdbc.drivers"));
+		System.out.println("\nDrivers loaded by DriverManager:");
+		Enumeration<Driver> list = DriverManager.getDrivers();
+		while (list.hasMoreElements())
+			System.out.println(list.nextElement());
+		
+	}
+}
