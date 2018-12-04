5e84e97e4ddae12a0f698d3c3248252f2f790a80
zOwenn <okhickman1@sheffield.ac.uk>
2018-12-04 15:14:30 +0000

    Adding constructors & get/set

--- MainProject/src/Models/Degree.java
+++ MainProject/src/Models/Degree.java
@@ -1,32 +1,98 @@
 package Models;
 
+import java.util.ArrayList;
+
 public class Degree {
 	private String code;
 	private String name;
+	private Integer level;
 	private Boolean industry;
 	private GraduateType type;
 	// Would represent the data in the separate table
 	private Department leadDept;
-	private Department[] assistingDepts;
+	private ArrayList<Department> assistingDepts;
 	
 	public Degree () {
 		
 	}
 	
-	public Degree (String c, String n, Boolean i, String gt, Department d) {
+	public Degree (String c, String n) {
 		code = c;
 		name = n;
+	}
+	
+	public Degree (String c, String n, Integer l, Boolean i, String gt, Department d) {
+		code = c;
+		name = n;
+		level = l;
 		industry = i;
 		type = GraduateType.valueOf(gt.toUpperCase());
 		leadDept = d;
 	}
 	
-	public Degree (String c, String n, Boolean i, String gt, Department ld, Department[] ad) {
+	public Degree (String c, String n, Integer l, Boolean i, String gt, Department ld, ArrayList<Department> ad) {
 		code = c;
 		name = n;
+		level = l;
 		industry = i;
 		type = GraduateType.valueOf(gt.toUpperCase());
 		leadDept = ld;
 		assistingDepts = ad;
+	}
+	
+	public void setCode(String c) {
+		code = c;
+	}
+	
+	public String getCode() {
+		return code;
+	}
+	
+	public void setName(String n) {
+		name = n;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setLevel(Integer l) {
+		level = l;
+	}
+	
+	public Integer getLevel() {
+		return level;
+	}
+	
+	public void setIndustry(Boolean i) {
+		industry = i;
+	}
+	
+	public Boolean getIndustry() {
+		return industry;
+	}
+	
+	public void setType(GraduateType gt) {
+		type = gt;
+	}
+	
+	public GraduateType getType() {
+		return type;
+	}
+	
+	public void setLead(Department d) {
+		leadDept = d;
+	}
+	
+	public Department getLead() {
+		return leadDept;
+	}
+	
+	public void setAssist(ArrayList<Department> ad) {
+		assistingDepts = ad;
+	}
+	
+	public ArrayList<Department> getAssist() {
+		return assistingDepts;
 	}
 }
